cbef362eadc8f40a1764099e9e73db5234b9c11e
Harsh-singhdei <harshsingh06a@gmail.com>
2024-06-08 21:21:45 +0530

    PUSHING THE BASIC CALCULATOR TO GITHUB

--- 2_calculator/2_calci.py
+++ 2_calculator/2_calci.py
@@ -1,36 +1,59 @@
 #                          A BASIC CALCULATOR TO PERFORM BASIC MATHEMATICS OPERATIONS
 
+def print_separator():
+    print("=" * 40)
 
-num1 = float(input("ENTER THE FIRST NUMBER: "))
+def get_number(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+def get_operation():
+    valid_operations = ["+", "-", "*", "/", "%", "**"]
+    while True:
+        operand = input("Enter the operation (+, -, *, /, %, **): ")
+        if operand in valid_operations:
+            return operand
+        else:
+            print("Invalid operation. Please enter a valid operation.")
+
+print_separator()
+print("WELCOME TO THE BASIC CALCULATOR")
+print_separator()
+
+num1 = get_number("Enter the first number: ")
 
 again = "y"
-while(again != "n"):
-    
-    
-    num2 = float(input("ENTER THE SECOND NUMBER: "))
-    operand = input("ENTER THE OPERATION: ")
+while again.lower() != "n":
+    num2 = get_number("Enter the second number: ")
+    operand = get_operation()
 
-    if(operand == "+"):
+    if operand == "+":
         answer = num1 + num2
-        print(answer)
-    elif(operand == "-"):
+    elif operand == "-":
         answer = num1 - num2
-        print(answer)
-    elif(operand == "*"):
+    elif operand == "*":
         answer = num1 * num2
-        print(answer)
-    elif(operand == "/"):
+    elif operand == "/":
+        if num2 == 0:
+            print("Error: Division by zero is not allowed.")
+            continue
         answer = num1 / num2
-        print(answer)
-    elif(operand == "%"):
+    elif operand == "%":
+        if num2 == 0:
+            print("Error: Modulo by zero is not allowed.")
+            continue
         answer = num1 % num2
-        print(answer)
-    elif(operand == "**"):
+    elif operand == "**":
         answer = num1 ** num2
-        print(answer)
-    else:
-        print("ENTER A VALID DATA ")
-    num1 = answer
-    print("YOU want to perform more operations then enter y -> yes otherwise enter n -> no")
-    again = input("")
 
+    print(f"The result of {num1} {operand} {num2} is: {answer}")
+    num1 = answer
+
+    print_separator()
+    again = input("Do you want to perform more operations? (y/n): ").strip().lower()
+    print_separator()
+
+print("Thank you for using the basic calculator!")
