2381328f8b6203a4c0590e2073dc60c31f087ee0
Harsh-singhdei <harshsingh06a@gmail.com>
2024-06-08 21:02:21 +0530

    MAKING VARIABLES GLOBAL

--- 1_rock_game/fourth.py
+++ 1_rock_game/fourth.py
@@ -9,7 +9,7 @@ lose = 0
 
 # FUNCTION FOR DISPLAYING ALL THE RULES
 def rules():
-    print("THE BASIC RULES OF THIS GAME ARE:-")
+    print("----:    THE BASIC RULES OF THIS GAME ARE    :----")
     print("1.   USER and computer have to choose out of three choices randomly")
     print("2.   ROCK BEATS SCISSORS")
     print("3.   SCISSORS BEATS PAPER")
@@ -22,7 +22,7 @@ def rules():
     print()
 
 # FUNCTION TO DISPLAY THE LEADERBOARD
-def leader_board(count,win,lose,tie):
+def leader_board(count):
     print("\t\t----:LEADERBOARD:----")
     print("Total Numbers of Games Played: |",count,"|")
     print("Total rounds won:              |",win,"|")
@@ -31,10 +31,11 @@ def leader_board(count,win,lose,tie):
     print()
 
 # FUNCTION FOR SCORE TRACKING    
-def SCORE_TRACKING(result,com_choice,count,win,lose,tie):
+def SCORE_TRACKING(result,com_choice):
     if result == 1:
         print("Your choice:     |", choice,"|")
         print("Computer choice: |", com_choice,"|")
+        global tie
         tie += 1
         print()
         print("Its a Tie!!")
@@ -42,6 +43,7 @@ def SCORE_TRACKING(result,com_choice,count,win,lose,tie):
     elif result == 2:
         print("Your choice:     |", choice,"|")
         print("Computer choice: |", com_choice,"|")
+        global lose
         lose += 1
         print()
         print("HARD Luck You Lost -_-")
@@ -49,15 +51,17 @@ def SCORE_TRACKING(result,com_choice,count,win,lose,tie):
     else:
         print("Your choice:     |", choice,"|")
         print("Computer choice: |", com_choice,"|")
+        global win
         win += 1
         print()
         print("BRAVO You won :) !!!")
         print()
+    global count
     count += 1
     return (count,win,lose,tie)
 
 # FUNCTION TO DECLARE THE RESULTS
-def game(choice,list,count,win,lose,tie):
+def game(choice,list):
     replay = "yes"
     while replay == "yes" or replay=="y":
         while(choice not in list):
@@ -91,7 +95,11 @@ def game(choice,list,count,win,lose,tie):
             else:
                 result = 3   
 
-        count,win,lose,tie = SCORE_TRACKING(result,com_choice,count,win,lose,tie)
+        global count
+        global lose
+        global win
+        global tie
+        count,win,lose,tie = SCORE_TRACKING(result,com_choice)
         
         replay = input("DO you want to play again(press : y for yes / n for no / L for leaderbord / r for rules )) : ").lower()
         print()
@@ -102,14 +110,13 @@ def game(choice,list,count,win,lose,tie):
         if(replay == "r"):
             print()
             rules()
-            leader_board(count,win,lose,tie)
+            leader_board(count)
             replay = input("Do you want to play again? (y for yes / n for no): ").lower()
-            continue
         if(replay=="l"):
-            leader_board(count,win,lose,tie)
+            leader_board(count)
             replay = input("Do you want to play again? (y for yes / n for no): ").lower()
         if(replay=="n"):
-            leader_board(count,win,lose,tie)
+            leader_board(count)
             return
         choice = input("Enter your choice(rock, paper or scissors): ").lower()
 
@@ -122,6 +129,6 @@ str = input().lower()
 if(str == "s"):
     print()
     choice = input("Enter your choice(rock, paper or scissors): ").lower()
-    game(choice,list,count,win,lose,tie)
+    game(choice,list)
 else:
     print("HOPE YOU COME AGAIN TO PLAY THIS EXCITED GAME :) ")
